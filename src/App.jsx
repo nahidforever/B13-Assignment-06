cd25cc49d016752411674f79f327efcb8d329bdc
Nahid1828 <isam15-5334@diu.edu.bd>
2026-03-29 22:49:54 +0600

    Design Navbar according to Figma

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <h1 className="text-4xl text-center">Assignment-6</h1>
+     <NavBar></NavBar>
     </>
   );
 }
